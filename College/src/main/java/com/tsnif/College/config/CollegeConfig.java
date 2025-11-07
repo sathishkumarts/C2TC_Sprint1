3eb8ff189415cab4b524d34632e595b255e7dbd6
Sathishkumarts <sathishkumarts0109@gmail.com>
2025-11-07 21:05:20 +0530

    Checked with Postman API

--- College/src/main/java/com/tsnif/College/config/CollegeConfig.java
+++ College/src/main/java/com/tsnif/College/config/CollegeConfig.java
@@ -1,22 +1,3 @@
-//package com.tnif.College.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//
-//@Configuration
-//@EnableWebMvc
-//public class CollegeConfig {
-//	
-//	public void addCorsMappings(CorsRegistry registry) {
-//		registry.addMapping("/**")
-//			.allowedOrigins("http://localhost:3000/")
-//			.allowedMethods("GET", "POST", "PUT", "DELETE");
-//	}
-//	
-//}
-
-
 
 
 package com.tsnif.College.config;
